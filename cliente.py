4a554e497fa93e3b92a2f93125edfca514bdb15d
JamerP11 <jdpachecoh@unadvirtual.edu.co>
2026-05-04 11:52:27 -0500

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,1 +1,17 @@
+from abc import ABC, abstractmethod
+from excepciones import ErrorValidacion
 
+class Entidad(ABC):
+    @abstractmethod
+    def mostrar_info(self):
+        pass
+
+class Cliente(Entidad):
+    def __init__(self, nombre, email):
+        if not nombre or not email:
+            raise ErrorValidacion("Datos del cliente inválidos")
+        self.__nombre = nombre
+        self.__email = email
+
+    def mostrar_info(self):
+        return f"Cliente: {self.__nombre} - {self.__email}"
